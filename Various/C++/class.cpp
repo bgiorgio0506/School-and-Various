62e3f8ca4942fc56838a04f550c581a660a407ec
bgiorgio0506 <bgiorgio0506@gmail.com>
2019-06-08 21:30:57 +0300

    rewriated class.cpp

--- Various/C++/class.cpp
+++ Various/C++/class.cpp
@@ -1,87 +1,91 @@
-#include "stdlib.h"
+//including libs
 #include "iostream"
 #include "string.h"
+using namespace std;
 
-namespace std::aeromobile{
-
+//calss object
 class aeromobile {
 private:
-  string type, engine, dep, dest;
+  string Ac, engine, dep, dest;
   int fuel, altitude_set, heading_set, radio_altimeter, thrust_set;
   bool takeoff, autopilot;
 
-  private void refuel(int) {
-    std::cout << "Insert refuel amount" << '\n';
-    std::cin >> fuel;
+  int refuel(int fuel) {
+    cout << "Insert refuel amount" << '\n';
+    cin >> fuel;
+    return fuel;
   }
 
 
 public:
- aeromobile(string, string, string, string, int);
-void fuel(int fuel) {
-  //if you have no fuel
-  if (fuel == 0){
-    for (size_t i = 0; i < 101; i++) {
-      fuel++;
-    }
-    std::cout << "sei stato rifornito" << '\n';
-  }
-  if (fuel<50) {
-    fuel1 = fuel
-    std::cout << "You are runnig out of fuel please refuel" << '\n';
-    fuel = refuel(fuel);
-    if (fuel1<fuel) {
-      std::cout << "Good job you refueled your aircraft" << fuel << '\n';
-    }
-  }
-  return fuel;
-}
 
-int flightplan(string dep, string dest) {
-if(strlen(dep)!=4 && strlen(dest)!=4){
-  std::cout << "Only ICAO code permitted" << '\n';
-}
-else
-//controllo db
-  if(dep==dest){
-    std::cout << "non puoi mettere lo stesso aeroporto " << '\n';
-  }
-  else{
-    string flightplan = dep + dest;
-    return flightplan
-  }
-}
+ aeromobile (string acf, string eng, int fuelAv, string depAirport, string destAirport){
+   Ac = acf;
+   engine = eng;
+   fuel = fuelAv;
+   dep = depAirport;
+   dest = destAirport;
+   cout << "Aircraft is initializing" << '\n';
+   system("clear");
+ }
 
+  void checkFuel() {
+    //if you have no fuel
+    if (fuel == 0){
+      for (size_t i = 0; i < 101; i++) {
+        fuel++;
+      }
+      cout << "You have been refueled" << '\n';
+    }
+    if (fuel<50) {
+      int fuel1 = fuel;
+      cout << "You are runnig out of fuel please refuel" << '\n';
+      fuel = refuel(fuel);
+      if (fuel1<=fuel) {
+        cout << "Good job you refueled your aircraft "<< '\n';
+      }else if(fuel1 > fuel){
+        cout << "Attention captain you have added a small ammount of fuel" << '\n';
+      }
+    }
+  }
+  //make flightPlan
+  string flightPlan() {
+  if(dep.length() !=4 || dest.length() !=4){
+    cout << "Only ICAO code permitted" << '\n';
+  }
+  else
+  //controllo db
+    if(dep==dest){
+      cout << "non puoi mettere lo stesso aeroporto " << '\n';
+    }
+    else{
+      string flightplan = dep +" - "+dest;
+      return flightplan;
+    }
+  }
 };
 
-aeromobile::aeromobile(string acf, string engine, string dep, string dest, int fuel){
-  type = acf;
-  engine = engine;
-  dep = dep;
-  dest = dest;
-  fuel = fuel;
-}
-int take_data_object(string acf, string engine, string dep, string dest,int fuel) {
-  std::cout << "Insert ACF(Aircraft)" << '\n';
-  std::cin >> acf;
-  std::cout << "Insert Engine" << '\n';
-  std::cin >> engine;
-  std::cout << "Insert fuel" << '\n';
-  std::cin >> fuel;
-  std::cout << "Let's make your flightplan" << '\n';
-  std::cout << "Enter Dep" << '\n';
-  std::cin >> dep;
-  std::cout << "Enter Dest" << '\n';
-  std::cin >> dest;
+//display menu and build the object
+void takeDataObject( string& ac, string& engineType, int& fuelLevel, string& depAirport, string& destAirport) {
+  cout << "Insert ACF(Aircraft)" << '\n';
+  cin >> ac;
+  cout << "Insert Engine" << '\n';
+  cin >> engineType;
+  cout << "Insert fuel" << '\n';
+  cin >> fuelLevel;
+  cout << "Let's make your flightplan" << '\n';
+  cout << "Enter Dep" << '\n';
+  cin >> depAirport;
+  cout << "Enter Dest" << '\n';
+  cin >> destAirport;
 
-   aeromobile B737(acf, engine, dep, dest, fuel);
-   return B737;
 }
-main(){
-  string acf,engine,dep,dest;
+
+int main(){
+  string acf , engine, dep, dest;
   int fuel;
-  B737= take_data_object(acf,engine,dep,dest,fuel);  std::cin >> dep;
-std::cout << "Il fuel è"<<aeromobile.fuel(fuel) << '\n';
-std::cout << "Il Piano di volo è "<<aeromobile.flightplan(dep,dest) << '\n';
-
+  takeDataObject(acf, engine, fuel, dep, dest);
+  aeromobile aereo(acf, engine, fuel, dep, dest);
+  aereo.checkFuel();
+  cout << "Il Piano di volo è "<<aereo.flightPlan() << '\n';
 }
